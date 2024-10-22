ed15e8ad3522c0c91427451a2938d7ba797fbe23
Steve Bosilovatz <steve@bosilovatz.com>
2024-10-22 08:20:37 -0500

    Class library with console app

---
+++ Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using Locator.Repositories;
+using System;
+using System.Data.SqlClient;
+using System.Data;
+
+IDbConnection dbConnection = new SqlConnection("Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;");
+
+var LocatorRepository = new LocatorRepository(dbConnection);
+//await LocatorRepository.GetClientList();
+
+Console.WriteLine("Hello, World!");
