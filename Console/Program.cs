8edcbdd0da6a23220b9c7eb497e01baf66da2f32
Steve Bosilovatz <steve@bosilovatz.com>
2024-10-24 08:22:11 -0500

    Progress

--- Console/Program.cs
+++ Console/Program.cs
@@ -6,10 +6,15 @@ IDbConnection dbConnection = new SqlConnection(
     "Server=localhost;Database=Locator;User Id=sa;Password=1StrongPwd!!;"
 );
 
-LocatorX locator = new(dbConnection);
+LocatorLib locator =
+    new(
+        dbConnection,
+        "dev-xshhwrh4f1vis6lb.us.auth0.com",
+        "auY2mDwrU8dPwlU8cMI4iLd2q4Lsu5nvhsF60DCRWk542AadNTQ8lV_jHp0MzRAF",
+        "RCbDL6LnErLJfuXz1s3hvLf6bVJklNFl"
+    );
+    
 var clients = await locator.GetClients();
-
-// print out the clients
 foreach (var client in clients)
 {
     Console.WriteLine($"{client.ClientId} - {client.ClientName} - {client.ClientCode}");
